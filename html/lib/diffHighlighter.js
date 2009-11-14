aac45d3303e447772065af582f8bbbf2debc5339
Hans Fugal <hans@fugal.net>
2009-11-13 20:29:54 -0800

    trailing whitespace on dellines

--- html/lib/diffHighlighter.js
+++ html/lib/diffHighlighter.js
@@ -187,11 +187,13 @@ var highlightDiff = function(diff, element, callbacks) {
 		}
 
 		sindex = "index=" + lindex.toString() + " ";
-		if (firstChar == "+") {
+		if (firstChar == "+" || firstChar == "-") {
 			// Highlight trailing whitespace
 			if (m = l.match(/\s+$/))
 				l = l.replace(/\s+$/, "<span class='whitespace'>" + m + "</span>");
+		}
 
+		if (firstChar == "+") {
 			line1 += "\n";
 			line2 += ++hunk_start_line_2 + "\n";
 			diffContent += "<div " + sindex + "class='addline'>" + l + "</div>";
